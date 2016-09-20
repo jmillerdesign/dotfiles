ae35775969b46c34b5256c8ae10337d6d342760a
J. Miller <j@thriveline.com>
2016-09-20 15:02:30 -0700

    Keep track of which casks are installed

---
+++ cask.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Install apps using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew cask update
+
+brew tap caskroom/versions
+brew tap caskroom/fonts
+
+brew cask install android-studio
+                  anonvpn \
+                  audacity \
+                  balsamiq-mockups \
+                  bartender \
+                  bettertouchtool \
+                  boot2docker \
+                  caffeine \
+                  cocoapods \
+                  diskwave \
+                  dockertoolbox \
+                  dropbox \
+                  eclipse-installer \
+                  firefox \
+                  flux \
+                  flycut \
+                  font-fontawesome \
+                  font-open-sans \
+                  font-roboto \
+                  font-source-code-pro \
+                  font-source-sans-pro \
+                  gimp \
+                  google-chrome \
+                  imageoptim \
+                  integrity \
+                  iterm2 \
+                  java \
+                  launchrocket \
+                  mongo-management-studio \
+                  mou \
+                  mysqlworkbench \
+                  qlmarkdown \
+                  quicklook-json \
+                  rdm \
+                  sitesucker \
+                  sketchup \
+                  skitch \
+                  skype \
+                  slack \
+                  spotify \
+                  sublime-text3 \
+                  textwrangler \
+                  the-unarchiver \
+                  tower \
+                  transmit \
+                  virtualbox \
+                  vlc
